6fe4cec1b113706647f0598f6207152441c668a6
ricardojaj <rickanjos85@gmail.com>
2025-08-25 09:12:04 -0300

    modification: script exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -6,9 +6,9 @@ cidade = 'Hortolandia'
 anos_futuro = idade + 10
 
 print("Nome: " + nome)
-print("Idade: " + idade)
-print("Altura: " + altura)
-print("Estudante: " + estudante)
+print("Idade: ", idade)
+print("Altura: ", altura)
+print("Estudante: ", estudante)
 print("Cidade: " + cidade)
 
-print("Daqui 10 anos a minha idade é: " + anos_futuro)
+print("Daqui 10 anos a minha idade é: ", anos_futuro)
