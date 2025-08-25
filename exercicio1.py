3c818e2383648618377979d1570805fa185b01b9
ricardojaj <rickanjos85@gmail.com>
2025-08-25 08:56:37 -0300

    create: script exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+nome = 'Ricardo'
+idade = 25
+altura = 1.79
+estudante = True
+cidade = 'Hortolandia'
+anos_futuro = idade + 10
+
+print("Nome: " + nome)
+print("Idade: " + idade)
+print("Altura: " + altura)
+print("Estudante: " + estudante)
+print("Cidade: " + cidade)
+
+print("Daqui 10 anos a minha idade é: " + anos_futuro)
