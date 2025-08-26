c0770fc2b00242ecb08114219b1d853e9749e402
ricardojaj <rickanjos85@gmail.com>
2025-08-26 17:50:54 -0300

    Create: script desafio1,py

---
+++ desafio1.py
@@ -0,0 +1,19 @@
+import math
+
+a = 1
+b = 8 
+c = -9
+
+
+if a < 0:
+  print("Δ é menor que zero - a equação não possui raízes reais!")
+else:
+        delta = (b * b) - 4 * a * c
+        x1 = (-b + math.sqrt(delta)) / 2
+        x2 = (-b - math.sqrt(delta)) / 2
+        print("Delta: "delta)
+        print("Primeira raiz: ", x1)
+        print("Segunda raiz: ", x2)
+        
+
+
