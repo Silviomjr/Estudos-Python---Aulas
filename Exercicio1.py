8ccef4a5c9bc909b21a67dbd206366cc4c25b11d
Aliny-QA <aliny.pinto@hotmail.com>
2025-08-26 14:50:31 -0300

    Create: script Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,9 @@
+nome = "Aliny"
+idade = 29
+altura = 1.54
+estudante = True
+cidade = "Serra"
+print('Nome: '+ nome +' idade: ', idade , ' altura: ', altura , ' Estudante: ' , estudante , ' Cidade: ' , cidade)
+
+anos_futuro = idade + 10
+print('Daqui 10 anos terei: ', anos_futuro)
