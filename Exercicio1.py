7a7cf6663d2fabd402e912590270b62624319bdc
Ana Reis <ana.reis1@br.ey.com>
2025-08-26 20:29:01 -0300

    create 1

---
+++ Exercicio1.py
@@ -0,0 +1,12 @@
+nome = "Ana Larissa"
+print (nome)
+idade = 26
+print (idade) 
+altura = 1.70
+print (altura)
+estudante = False
+print (estudante)
+cidade = "Salto"
+print (cidade)
+idade_futura = (idade) + 10
+print (idade_futura)
