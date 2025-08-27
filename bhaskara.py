b5c6693de8f7a01148c3a38e29f105993da8b3cc
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-08-27 08:02:09 -0300

    create:ex01.py

---
+++ bhaskara.py
@@ -0,0 +1,24 @@
+a = input("Digite a: ")
+b = input("Digite b: ")
+c = input("Digite c: ")
+
+try:
+    a = float(a)
+    b = float(b)
+    c = float(c)
+except:
+    print("Insira Numeros")
+
+delta = (b**2) - (4*a*c)
+
+try:
+    delta > 0
+    x1 = (-b + delta) / (2*a)
+    x2 = (-b - delta) / (2*a)
+    #Não tenho ideia de como prosseguir com Raiz em Python
+    print("Δ:", delta)
+    print("X1:", x1)
+    print("X2:", x2)
+    print("Raízes reais encontradas.")
+except:
+    print("Δ é menor que zero - a equação não possui raízes reais!")
---
+++ ex01.py
@@ -0,0 +1,14 @@
+nome = "Rafael"
+idade = 28
+altura = 1.76
+estudante = True
+cidade = "Rio Claro"
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Estudante:", estudante)
+print("Cidade:", cidade)
+
+anos_futuro = idade + 10
+print("Idade em 10 anos:", anos_futuro)
