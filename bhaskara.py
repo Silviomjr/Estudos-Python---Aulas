bef4fbdf500b7988856762ff4d4dec798556a676
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-08-27 09:06:11 -0300

    modify: bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -14,15 +14,15 @@ except:
 delta = (b**2) - (4*a*c)
 
 try:
-    delta >= 0
-    x1 = (-b + (delta**0.5)) / (2*a)
-    x2 = (-b - (delta**0.5)) / (2*a)
+    if delta >= 0:
+        x1 = (-b + (delta**0.5)) / (2*a)
+        x2 = (-b - (delta**0.5)) / (2*a)
     #Não tenho ideia de como prosseguir com Raiz em Python
-    #Copiei os amiguinhos na cara dura
-    print("Δ:", delta)
-    print("X1:", x1)
-    print("X2:", x2)
-    print("Raízes reais encontradas.")
+    #Copiei os amiguinhos na cara dura4
+        print("Δ:", delta)
+        print("X1:", x1)
+        print("X2:", x2)
+        print("Raízes reais encontradas.")
     if delta != 0:
         print("As raizes são diferentes de zero")
 except:
