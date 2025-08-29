19dbaa0f525ef65094c63f6caeed2ce783cdd070
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-08-29 08:51:29 -0300

    modify: ex01extra.py

---
+++ ex01extra.py
@@ -0,0 +1,15 @@
+nome = "Rafael"
+idade = 28
+altura = 1.76
+eh_estudante = True 
+
+print ("\n Nome:", nome, "\n Idade:", idade, "\n Altura:", altura, "\n Estudante:", eh_estudante)
+
+ano_nascimento = 2025-idade
+idade = idade + 5
+
+eh_estudante = not eh_estudante
+
+altura_cm = altura * 100
+
+print ("\n Idade: ", idade, "\n Estudante", eh_estudante, "\n Ano Nascimento:", ano_nascimento, "\n Altura em Cm:", altura_cm)
