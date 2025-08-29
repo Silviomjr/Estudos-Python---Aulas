a94643ca85d0eb0dfb24173441952ae54a92deab
Alef Moreira <Alef.Moreira@br.ey.com>
2025-08-29 09:08:57 -0300

    exercicio: declaracao de variaveis

---
+++ exercicio-extra1.py
@@ -0,0 +1,19 @@
+nome = "Alef"
+idade = 30
+altura = 1.78
+eh_estudante = True
+
+print('nome:', nome)
+print('Idade:', idade)
+print('altura:', altura)
+print('eh_estudante:', eh_estudante)
+ano_de_nascimento = 2025 - idade
+idade = idade + 5
+eh_estudante = not eh_estudante
+
+altura_em_cm = int(altura * 100)
+print('Ano de nascimento:', ano_de_nascimento)
+print('Idade daqui a 5 anos:', idade)
+print('Altura em cm:', altura_em_cm)
+print('eh_estudante:', eh_estudante)
+
