c7481f576b68888eebee0f719d65e0eace763f3e
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-08-29 08:23:42 -0300

    modify: bhaskara.py

--- bhaskara.py
+++ bhaskara.py
@@ -1,4 +1,5 @@
 import sys
+
 a = input("Digite a: ")
 b = input("Digite b: ")
 c = input("Digite c: ")
@@ -11,20 +12,20 @@ except:
     print("Insira Numeros Validos")
     sys.exit()
 
-delta = (b**2) - (4*a*c)
+if a == 0:
+    print("Não é uma equaçao de segundo grau")
+    sys.exit()
+else:
 
-try:
+    delta = (b**2) - (4*a*c)
+
     if delta >= 0:
         x1 = (-b + (delta**0.5)) / (2*a)
         x2 = (-b - (delta**0.5)) / (2*a)
-    #Não tenho ideia de como prosseguir com Raiz em Python
-    #Copiei os amiguinhos na cara dura4
+
         print("Δ:", delta)
         print("X1:", x1)
         print("X2:", x2)
         print("Raízes reais encontradas.")
-    if delta != 0:
-        print("As raizes são diferentes de zero")
-except:
-    print("Δ é menor que zero - a equação não possui raízes reais!")
-    sys.exit()
+    else:
+        print("Delta é menor que zero, equação não possui raizes reais.")
