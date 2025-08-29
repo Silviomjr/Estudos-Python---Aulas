926f577f302f08064e842506aedf2059176439bd
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-08-29 10:18:06 -0300

    create: ex02extra.py

---
+++ ex02extra.py
@@ -0,0 +1,35 @@
+idade = 28
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+numero = 10
+
+if numero >= 0:
+    print("Número Positivo")
+else:
+    print("Numero Negativo")
+
+
+senha = "silvioaula123"
+
+if len(senha) >= 8:
+    print("Senha forte o suficiente")
+#Usei o Copilot pra checar isso
+else:
+    print("Senha fraca")
+
+temperatura = 18
+
+if temperatura >= 30:
+    print("Está quente lá fora")
+else:
+    print("Está frio lá fora")
+
+nota = 100
+
+if nota >= 60:
+    print("Aprovado")
+else:
+    print("Reprovado")
