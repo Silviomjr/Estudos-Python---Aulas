d46610c503f64ad85f05632c53ecead42056ffbb
Aliny-QA <aliny.pinto@hotmail.com>
2025-08-30 11:34:36 -0300

    Create: Exercicio extra 2

---
+++ exercicio-extra2.py
@@ -0,0 +1,13 @@
+idade = 20
+
+if idade >= 0:
+    print('Você é maior de idade')
+
+numero = 30
+
+if numero > 0:
+    print('O número é positivo!')
+
+senha = "Senha123"
+print(len(senha))
+if senha 
