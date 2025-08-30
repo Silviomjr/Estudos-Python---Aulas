22c8f13418286d1fba1793543b81c8c7c201fd87
Aliny-QA <aliny.pinto@hotmail.com>
2025-08-30 11:48:22 -0300

    Create Exercico extra 2

--- exercicio-extra2.py
+++ exercicio-extra2.py
@@ -1,13 +1,28 @@
+#Tarefa 1
 idade = 20
 
 if idade >= 0:
     print('Você é maior de idade')
 
+#Tarefa 2
 numero = 30
 
 if numero > 0:
     print('O número é positivo!')
 
+#Tarefa 3
 senha = "Senha123"
-print(len(senha))
-if senha 
+
+if len(senha) >= 8:
+   print("Senha é forte o suficiente")
+
+#Tarefa 4
+temperatura = 31
+
+if temperatura > 30:
+  print('Etá quente lá fora!')
+
+#Tarefa 5
+nota = 70
+if nota >= 60:
+   print('Parabéns! Você passou!')
