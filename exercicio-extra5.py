9c78c62333fa9e298f2c8910aa099ae45d12ed00
Aliny-QA <aliny.pinto@hotmail.com>
2025-09-01 08:20:10 -0300

    Create: script exercicio-extra5.py

---
+++ exercicio-extra5.py
@@ -0,0 +1,34 @@
+# #Tarefa 1
+dividendo = 12
+divisor = 0
+
+try:
+    divisao = dividendo / divisor
+    print('Resultado da divisão é: ', divisao)
+except ZeroDivisionError:
+    print('Não é possível dividir por zero!')
+
+
+# #Tarefa 2
+dividendo = 12
+divisor = 0
+
+try:
+    divisao = dividendo / divisor
+    print('Resultado da divisão é: ', divisao)
+except ZeroDivisionError:
+    print('Não é possível dividir por zero!')
+
+
+# #Tarefa 3
+numeros = [10, 20, 30]
+
+try:
+    indice = numeros [5]
+    print('Elemento: ', indice)
+except:
+    print('Índice fora do alcance')
+
+# #Tarefa 4
+
+# try:
