43557cb034c300c33165adee75b5e1f9922f5a3a
ricardojaj <rickanjos85@gmail.com>
2025-09-02 18:36:53 -0300

    Modification: script desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -4,17 +4,23 @@ try:
     a = float(input("Digite o valor de A: "))
     b = float(input("Digite o valor de B: "))
     c = float(input("Digite o valor de C: "))
-    if a < 0:
-        print("Δ é menor que zero - a equação não possui raízes reais!")
-    else:
-        delta = (b * b) - 4 * a * c
+except ValueError:
+    print("Valor digitado é invalido!")
+
+
+if a == 0:
+    print("Não é uma equação de segundo gau")
+else:
+    delta = b**2 - (4 * a * c)
+        
+    if delta >= 0:
         x1 = (-b + math.sqrt(delta)) / 2
         x2 = (-b - math.sqrt(delta)) / 2
         print("Delta: ", delta)
         print("Primeira raiz: ", x1)
         print("Segunda raiz: ", x2)
-except ValueError:
-    print("Valor digitado é invalido!")
+    else:
+        print("Delta é menor que zero, equação não possui raizes reais.")
         
 
 
