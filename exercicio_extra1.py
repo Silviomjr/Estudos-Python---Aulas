c9cf3b6163f016605e2c558452054d17872902cf
ricardojaj <rickanjos85@gmail.com>
2025-09-06 15:24:56 -0300

    create: script exercicio_Extra1

---
+++ exercicio_extra1.py
@@ -0,0 +1,18 @@
+nome = 'Ricardo'
+idade = 25
+altura = 1.79
+eh_estudante = True
+
+print("Meu nome é: ", nome)
+print("Idade: ", idade)
+print("altura: ", altura)
+print("Estudante: ", eh_estudante)
+
+idade = idade + 5
+eh_estudante = False
+
+ano_nascimento = 2025 - 25
+altura_cm = altura * 100
+
+print("Seu ano de nascimento: ", ano_nascimento)
+print("Sua altura de metros para centimetro: ", altura_cm)
