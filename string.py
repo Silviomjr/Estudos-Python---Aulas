eea17f02d7e28138c2a430953b1c3b524352303e
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-09-12 14:38:31 -0300

    create string.py

---
+++ string.py
@@ -0,0 +1,15 @@
+string = 'X-DSPAM-Confidence: 0.8475'
+
+apos = string.find(':')
+apos = string[apos + 1:]
+apos = float(apos)
+print(apos)
+
+
+string2 = '   Olá Mundo   '
+string2 = string2.strip()
+print(string2)
+
+string3 = 'Olá Silvio'
+string4 = string3.replace('Silvio', 'Rafael')
+print(string4)
