c3df4769e8b793308e6a50b6d3bd33e1b23268b8
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-09-12 08:27:07 -0300

    modify bananaana.py

--- bananaana.py
+++ bananaana.py
@@ -1,4 +1,13 @@
 palavra = 'banana'
-contar = palavra.split("ana")
-total = len(contar)
-print(total)
+#contar = palavra.split("ana")
+#total = len(contar)
+#print(total)
+
+count = 0
+for index, letra in enumerate(palavra):
+    anamagrama = palavra[index: index + 3]
+    if anamagrama.lower() == 'ana':
+        print(f"Achei {anamagrama} na posição {index}")
+        count = count + 1
+
+print(count)
