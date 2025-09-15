70b9ee761564f7a9f7267c9c44955848d01b2257
Ana Reis <ana.reis1@br.ey.com>
2025-09-15 08:49:13 -0300

    replace

---
+++ Exercicio3pt2.py
@@ -0,0 +1,4 @@
+Numero = '1 2 3 4 5 6'
+Letras = Numero.replace ('1','A').replace('2','B',).replace('3','C').replace('4','D').replace('5','E').replace('6','F')
+print("Numeros: ",Numero)
+print("Letras: ",Letras)
