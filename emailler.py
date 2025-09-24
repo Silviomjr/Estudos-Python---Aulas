0a58ea87dacde4e7a2a1200fe6cea26072cb6dc2
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-09-24 08:30:58 -0300

    modify emailler.py

--- emailler.py
+++ emailler.py
@@ -3,13 +3,13 @@ import sys
 
 file = open('C:\Projetos\Exercicios 1\Email.txt', 'r')
 #file = open("Email.txt", "r")
-#mypath = os.path.abspath(os.path.dirname(__file__))
+#mypath = os.path.abspath(os.path.dirname(__file__))+'\Email.txt'
+#print(mypath)
+emails = [] 
 
-for palavra in file.read().split():
-    if "@br.ey.com" in palavra:
-        print(palavra)
-    else:
-        print("Não é um email")
-    
-print(file.read())
+for palavra in file:
+    if "vale" in palavra.lower():
+        emails.append(palavra)
+
+print(emails)
 file.close()
