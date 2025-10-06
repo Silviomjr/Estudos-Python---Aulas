52563eab2e47c35478018ea97b66a6b47758b6e6
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-10-06 14:55:05 -0300

    create listinha.py

--- listinha.py
+++ listinha.py
@@ -2,6 +2,10 @@ lista = [10, 'ana', [2,5], 7]
 
 lista2 = lista[1]
 
-print(lista2) 
+acheana = 'ana' in lista
+ache10 = 5 in lista
 
+print(acheana)
+print(ache10)
+print(lista2) 
 print(lista[2][0])
