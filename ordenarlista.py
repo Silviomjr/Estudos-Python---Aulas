a1e62d2c9ff3ec76da1de297edf08a53cc009b7b
Ana Reis <ana.reis1@br.ey.com>
2025-10-20 08:07:48 -0300

    update

--- ordenarlista.py
+++ ordenarlista.py
@@ -3,6 +3,25 @@ ordenada = sorted(lista, reverse = True)
 print("Lista original:", lista)
 print("Lista ordenada:", ordenada)
 
+def acharMaior (listaSuporte):
+    maior = listaSuporte[0]
+    for i in range(1, len(listaSuporte)):
+        if listaSuporte[i] > maior:
+            maior = listaSuporte[i]
+    return maior
+print("O maior valor da lista é:", acharMaior(lista))
+
+def funcaoOrdenar(lista):
+    listaOrdenada = []
+    listaCopia = lista.copy()
+    for x in range(len(lista)):
+        maiorvalor = acharMaior(listaCopia)
+        listaOrdenada.append(maior)
+        indexDeOcorrencia = listaCopia.index(maiorvalor)
+        listaCopia.pop(indexDeOcorrencia)
+    return listaOrdenada
+print("Lista ordenada pela função:", funcaoOrdenar(lista))
+
 
 # lista = [64, 34, 25, 12, 22, 11, 90, 5]
 
