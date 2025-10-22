20bf55da781a3a3f2e21c9fde950cb958bb15e4f
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-10-22 08:07:23 -0300

    modify listanome.py

--- listanome.py
+++ listanome.py
@@ -6,10 +6,10 @@ dicionarioContagem = dict()
 for nome in ListaNome:
     #Adicionar Lower
     nome = nome.lower()
-
+    #Verficar se o nome já está no dicionário
     if nome not in dicionarioContagem:
         dicionarioContagem[nome] = 1
     else:
         dicionarioContagem[nome] += 1
-
+#Printar o dicionário com as contagens
 print(dicionarioContagem)
