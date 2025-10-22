63077a43e9ebec82f63e5984db9dc877c68d9c2d
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-10-22 15:22:36 -0300

    modify listanome.py

--- listanome.py
+++ listanome.py
@@ -3,7 +3,7 @@ listaNome = ['marquard', 'zhen', 'cwen', 'csev', 'zhen','MARQUARD', 'Marquard']
 # Loop para contar a ocorrência de cada nome na lista
 
 def  count_nome(lista):
-
+    #Instanciar Variavel de dicionario vazio na função
     dicionarioContagem = dict()
 
     for nome in lista:
@@ -11,11 +11,14 @@ def  count_nome(lista):
         nome = nome.lower()
         #Verficar se o nome já está no dicionário
         if nome not in dicionarioContagem:
+            #Adiciona a primeira vez no dict() vazio
             dicionarioContagem[nome] = 1
         else:
+            #Faz a contagem +1
             dicionarioContagem[nome] += 1
 #Printar o dicionário com as contagens
     return dicionarioContagem
 
+#Chamar Função printando listaNome original
 print(count_nome(listaNome))
 
