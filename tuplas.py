fbe1b88c78ab65aea3bb157da47502b8006f393d
RafaelZulzke <rafaelzulzke@hotmail.com>
2025-10-27 15:36:50 -0300

    modify tuplas.py

--- tuplas.py
+++ tuplas.py
@@ -1,5 +1,9 @@
 my_dict = {'apple': 2, 'banana': 3, 'orange': 1, 'grape': 4, 'kiwi': 2}
+print("Dicionário original:", my_dict)
 
+#Chaves Ordenadas
+sorted_k_items = sorted(my_dict.items())
+print("Dicionário ordenado por chaves:", dict(sorted_k_items))
 #VaLores Crescentes
 sorted_items_asc = sorted(my_dict.items(), key=lambda item: item[1])
 sorted_dict_asc = dict(sorted_items_asc)
